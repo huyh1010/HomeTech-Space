d86f3426fd2b755c211bbe07dc6a5e89f52056ea
unknown <huyhuynh1010@gmail.com>
2023-08-11 16:14:00 +0700

    redirect to client link fixed

--- passport/redirectToClient.js
+++ passport/redirectToClient.js
@@ -5,7 +5,9 @@ const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 const redirectToClient = catchAsync(async (req, res, next) => {
   let googleId = req.user.googleId;
   googleId = jwt.sign({ _id: googleId }, JWT_SECRET_KEY);
-  res.redirect(`http://localhost:3000?googleId=${googleId}`);
+  res.redirect(
+    `https://hometech-space-huy-huynh.onrender.com/?googleId=${googleId}`
+  );
 });
 
 module.exports = redirectToClient;
