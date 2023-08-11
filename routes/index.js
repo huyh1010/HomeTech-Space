70bcd1f2a09f89150e85d9ea30f0b6d2dd21c1bf
unknown <huyhuynh1010@gmail.com>
2023-08-11 15:09:25 +0700

    route index fixed

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.status(200).send("Welcome to CoderSchool!");
+  res.status(200).send("Welcome to HomeTech Space!");
 });
 
 router.get("/template/:test", async (req, res, next) => {
